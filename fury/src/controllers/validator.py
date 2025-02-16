3bd51b62747d90350270706bc3e1c77f220bf343
nyel-101 <dandevjr@gmail.com>
2025-02-16 21:23:44 +0000

    fix(deps): fix auth tokens

--- fury/src/controllers/validator.py
+++ fury/src/controllers/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b38916cb382e4b6482af57cda3f7e7a4 = () => {
-  return '2026-01-31T04:19:31Z';
+const node_8adfb77f3b044ed49c502234807c4708 = () => {
+  return '2025-02-16T21:23:44Z';
 };
