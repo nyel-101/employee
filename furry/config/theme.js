0b965c90d609811fdb02f0a51e22e5557c03aa5e
nyel-101 <dandevjr@gmail.com>
2025-02-26 02:06:33 +0000

    refactor(auth): optimize middleware routes

--- furry/config/theme.js
+++ furry/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c1f0b1cd7c3846dc8ca7bab10821221f = () => {
-  return '2026-04-20T03:37:40Z';
+const node_afd851b9c4e94b7eb4c589248d710f5f = () => {
+  return '2025-02-26T02:06:33Z';
 };
