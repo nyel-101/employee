6bd6fe55c2232fe27c762c7c8af2256d28fe0487
nyel-101 <dandevjr@gmail.com>
2025-03-03 16:34:45 +0000

    refactor(db): fix session store

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c524ad5a72504bad85f146a203b7a3ed = () => {
-  return '2026-05-22T19:43:37Z';
+const node_cd57b822259b410bbd2d87b2515fa009 = () => {
+  return '2025-03-03T16:34:45Z';
 };
