4aa7c3e28986a996519e22a9a265e1ae3bfe2389
nyel-101 <dandevjr@gmail.com>
2025-03-25 10:38:45 +0000

    refactor(utils): fix database index

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_03f344e5596141b4bcedfe9086b4826a = () => {
-  return '2025-02-12T15:25:55Z';
+const node_4f66a2f0e40d4ee18d94677b307a0f1d = () => {
+  return '2025-03-25T10:38:45Z';
 };
