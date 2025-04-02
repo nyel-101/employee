54486f355871fbad71f60270749debab1c4e28b6
nyel-101 <dandevjr@gmail.com>
2025-04-02 08:14:01 +0000

    refactor(api): fix auth tokens

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5ebf752938964cd4a85d9ffb2eabd4c6 = () => {
-  return '2026-05-04T21:05:09Z';
+const node_cd6bc99f20e141fab4d20676f1ce0d29 = () => {
+  return '2025-04-02T08:14:01Z';
 };
