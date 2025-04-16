fed715707c99dfa663762e122bafa2c6194f49b1
nyel-101 <dandevjr@gmail.com>
2025-04-16 09:10:27 +0000

    chore(worker): decouple logging interface

--- fury/src/controllers/helper.py
+++ fury/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_acfeb26168a1477f83b674ce5e4711ec = () => {
-  return '2026-05-14T15:28:34Z';
+const node_ace15c23330644de90e335c96c016328 = () => {
+  return '2025-04-16T09:10:27Z';
 };
