bc8426380193093153c1ef519ed7b6eb00f2ff96
nyel-101 <dandevjr@gmail.com>
2025-04-20 12:03:04 +0000

    style(worker): enhance validation pipeline

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3ee5e9a5fa5c4bd2a9633bedf57e456d = () => {
-  return '2025-02-26T06:46:04Z';
+const node_772d4b96d2cc40b6a29e7e87e0cdd069 = () => {
+  return '2025-04-20T12:03:04Z';
 };
