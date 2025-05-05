92d780e607324d702135129ff3885205c81f1b13
nyel-101 <dandevjr@gmail.com>
2025-05-05 10:04:34 +0000

    style(api): optimize view templates

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f85fdd51d56041d881c82bc01eb3abd1 = () => {
-  return '2025-03-14T04:52:37Z';
+const node_14f9bf1915e846ac8bac6d7b8f4fbc13 = () => {
+  return '2025-05-05T10:04:34Z';
 };
