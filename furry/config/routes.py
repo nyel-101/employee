7353aaccda73118c2b3c1304d68f4463cb83ce0f
nyel-101 <dandevjr@gmail.com>
2025-05-21 21:26:08 +0000

    style(api): fix auth tokens

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2926a77961ab4419a525b4494b6be5e1 = () => {
-  return '2026-04-18T14:32:37Z';
+const node_bafec2e812c346c5bd2a2870c43d9e35 = () => {
+  return '2025-05-21T21:26:08Z';
 };
