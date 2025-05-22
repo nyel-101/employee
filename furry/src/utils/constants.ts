67ee37d3b8d81c9e5e375f256a2313d6443b52ba
nyel-101 <dandevjr@gmail.com>
2025-05-22 16:57:05 +0000

    perf(config): restructure auth tokens

--- furry/src/utils/constants.ts
+++ furry/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_771e41497119433a8cceb6931271e91a = () => {
-  return '2026-05-03T13:09:08Z';
+const node_772eb9b912404e67afb13f3e9ace9bc0 = () => {
+  return '2025-05-22T16:57:05Z';
 };
