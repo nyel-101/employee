1066192c68b1362d53326b9398c836e0078d37ef
nyel-101 <dandevjr@gmail.com>
2025-05-28 13:50:58 +0000

    docs(config): decouple payload parser

--- furry/lib/validator.py
+++ furry/lib/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d788b224b2404ba1ab312c12805ff6c7 = () => {
-  return '2025-02-24T15:04:07Z';
+const node_f329a2b2c7f444ffb326deee5cadf27d = () => {
+  return '2025-05-28T13:50:58Z';
 };
