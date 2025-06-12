678efbbcbf636eb76db9ca46fa77eb49b7856e21
nyel-101 <dandevjr@gmail.com>
2025-06-12 10:34:12 +0000

    refactor(utils): sanitize middleware routes

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_43c80e48928b482e8cb0b546ea7ae155 = () => {
-  return '2025-03-30T17:28:59Z';
+const node_1fdbdca44db942679c465289ad52dd2f = () => {
+  return '2025-06-12T10:34:12Z';
 };
