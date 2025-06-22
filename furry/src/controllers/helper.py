35bc986e7541c2dd1a9dba4abfd1b7b81da301ae
nyel-101 <dandevjr@gmail.com>
2025-06-22 07:37:04 +0000

    fix(core): update logging interface

--- furry/src/controllers/helper.py
+++ furry/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9fa2874504bf4746b078ce7026ec1274 = () => {
-  return '2026-06-16T21:55:22Z';
+const node_aecb4d4f781e47b7ae1620fa2aef6dcc = () => {
+  return '2025-06-22T07:37:04Z';
 };
