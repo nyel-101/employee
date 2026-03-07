86c7ddb6a1caff2041bcff6bcf43169f5969f4c9
nyel-101 <dandevjr@gmail.com>
2026-03-07 11:32:31 +0000

    chore(config): fix auth tokens

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_67fa39962dd049a59436784900a9e2ec = () => {
-  return '2026-01-11T07:58:58Z';
+const node_f6dfaa07619548abb373e5f1fff09f91 = () => {
+  return '2026-03-07T11:32:31Z';
 };
