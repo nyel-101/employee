a401f1bb3852bea9ea09de3edaa38945d327c8a6
nyel-101 <dandevjr@gmail.com>
2026-03-11 07:01:21 +0000

    feat(api): restructure auth tokens

--- fury/src/controllers/theme.py
+++ fury/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_207f64bc1487430584ebed5a6e5faee3 = () => {
-  return '2026-03-08T02:01:51Z';
+const node_654049eb24914dbdbed20af84b898619 = () => {
+  return '2026-03-11T07:01:21Z';
 };
