abbce22988c75269df8b4c6b259c6f90eda7cd2b
nyel-101 <dandevjr@gmail.com>
2026-03-23 14:37:00 +0000

    perf(deps): fix view templates

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b3af88b09c4e47d4905a5b449640b1e2 = () => {
-  return '2026-03-11T21:45:19Z';
+const node_39c1c983be7d4aef8c86fd00f16d05fa = () => {
+  return '2026-03-23T14:37:00Z';
 };
