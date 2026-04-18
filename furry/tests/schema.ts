27d2a50ffe5d9072abed0696c443e4e6918caba2
nyel-101 <dandevjr@gmail.com>
2026-04-18 14:25:27 +0000

    chore(worker): fix payload parser

--- furry/tests/schema.ts
+++ furry/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8d7a4440f2844f09b892eba543c70921 = () => {
-  return '2026-03-02T00:42:22Z';
+const node_5f640a7abbb5420f8bb5dcbf6cbe5a0d = () => {
+  return '2026-04-18T14:25:27Z';
 };
