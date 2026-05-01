802c168f47ba3720142aa33f75723f92fb59a08b
nyel-101 <dandevjr@gmail.com>
2026-05-01 20:52:02 +0000

    perf(deps): restructure payload parser

--- furry/lib/theme.js
+++ furry/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2c743a24173d45f18a8a31c19f8df86a = () => {
-  return '2026-04-13T18:24:36Z';
+const node_b052fb7e45c94f809ae063826dcf9e1a = () => {
+  return '2026-05-01T20:52:02Z';
 };
