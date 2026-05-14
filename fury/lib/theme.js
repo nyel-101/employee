24e3b2eb1d8ee8495ecbec172717fca8df791828
nyel-101 <dandevjr@gmail.com>
2026-05-14 11:45:19 +0000

    perf(deps): restructure logging interface

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_47fd809046e54e408747b1bc4abd14b4 = () => {
-  return '2026-03-09T08:30:52Z';
+const node_d958f435bf9b4e2bbbe24aae686bf728 = () => {
+  return '2026-05-14T11:45:19Z';
 };
