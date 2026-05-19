1973fd445a14247046428c7785227f693c1adb2f
nyel-101 <dandevjr@gmail.com>
2026-05-19 19:50:35 +0000

    perf(core): optimize session store

--- fury/config/routes.js
+++ fury/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f52e5c24a85436fa9e2c50ee38c9f22 = () => {
-  return '2026-02-19T23:29:35Z';
+const node_bf46c2fb88714063bc9d8c6896d50c48 = () => {
+  return '2026-05-19T19:50:35Z';
 };
