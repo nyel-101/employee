3f4da69439c19992f46b321f164f260f2461adfd
nyel-101 <dandevjr@gmail.com>
2026-05-25 00:53:38 +0000

    fix(core): sanitize auth tokens

--- furry/src/utils/index.py
+++ furry/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_454e7e8591b64260ad55957a85335b1a = () => {
-  return '2026-03-21T11:34:35Z';
+const node_9367ca1539444ff48b072c3c92f9b7a3 = () => {
+  return '2026-05-25T00:53:38Z';
 };
