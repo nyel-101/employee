5128a909f0b97abf8dc6f3677e736dc8b4dc8eee
nyel-101 <dandevjr@gmail.com>
2026-06-13 08:32:27 +0000

    style(core): implement logging interface

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_73b4e2576f9541f69e048923b7b45428 = () => {
-  return '2026-06-11T17:44:37Z';
+const node_7ccf9e2acdb446c8910790aa3d1f70e3 = () => {
+  return '2026-06-13T08:32:27Z';
 };
