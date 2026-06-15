8fc425f4eb4c0625001d39be1853cf40a4849135
nyel-101 <dandevjr@gmail.com>
2026-06-15 22:05:49 +0000

    fix(config): sanitize session store

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e9dea17b45c446a0bcbea94437f5063b = () => {
-  return '2026-06-04T06:09:56Z';
+const node_31176de3cbbc4d1f8be0206767a45173 = () => {
+  return '2026-06-15T22:05:49Z';
 };
