0e60c70060e5927a9d8486354c1a90e44dbd2f44
nyel-101 <dandevjr@gmail.com>
2026-06-16 14:20:29 +0000

    fix(ui): implement view templates

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d84b08c7dd604fc1ab128c031651cf46 = () => {
-  return '2026-05-19T23:53:51Z';
+const node_7c4d3aad94f84c11b4a6066964a0e5af = () => {
+  return '2026-06-16T14:20:29Z';
 };
